c02703481c11b519cca83c666db47486602a3f74
yulimmm <89307538+yulimmm@users.noreply.github.com>
2023-02-16 19:29:17 +0900

    Update simple_nav_correction.cpp

--- simple_navigation_goals/src/simple_nav_correction.cpp
+++ simple_navigation_goals/src/simple_nav_correction.cpp
@@ -7,6 +7,8 @@
 #include <std_msgs/String.h>
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <sensor_msgs/MagneticField.h>
+#include <sensor_msgs/Imu.h>
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 ros::Publisher cmd_vel_publisher;
@@ -19,10 +21,16 @@ void down();
 void left();
 void pose_callback(const nav_msgs::Odometry data);
 void correction(double current_ori_z);
+void imu_callback(const sensor_msgs::Imu data);
 
 double current_x;
 double current_y;
 double current_ori_z;
+
+float imu_ori_x;
+float imu_ori_y;
+float imu_ori_z;
+float imu_ori_w;
 
 int count = 0;
 
@@ -38,13 +46,12 @@ int main(int argc, char** argv){
     ROS_INFO("Waiting for the move_base action server to come up");
   }
 
-  move_base_msgs::MoveBaseGoal goal;
+  move_base_msgs::MoveBaseGoal goal; 
   geometry_msgs::Twist cmd_vel;
   cmd_vel_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel",100,true);
 
-
-
   ros::Subscriber pose_sub = n.subscribe("/t265/odom/sample",10, pose_callback);
+  //ros::Subscriber mag_sub = n.subscribe("/imu/data",10, imu_callback); 
   ros::Subscriber min_distance_sub = n.subscribe("/min_distance",10, callback);
 
   ros::spin();
@@ -57,6 +64,16 @@ void pose_callback(const nav_msgs::Odometry data)
   current_x = data.pose.pose.position.x;
   current_y = data.pose.pose.position.y;
   current_ori_z = data.pose.pose.orientation.z;
+}
+
+void imu_callback(const sensor_msgs::Imu data)
+{
+  imu_ori_x = data.orientation.x;
+  imu_ori_y = data.orientation.y;
+  imu_ori_z = data.orientation.z;
+  imu_ori_w = data.orientation.w;
+
+  ROS_INFO("imu_data orientation \nx:%f y:%f z:%f w:%f",imu_ori_x,imu_ori_y,imu_ori_z,imu_ori_w);
 }
 
 void turn_right()
@@ -89,7 +106,7 @@ void turn_right()
     ROS_INFO("The base failed to turn left for some reason");
 
   //direction compensation 
-  if(count%5==0){
+  if(count%3==0){
     ROS_INFO("current_ori_z: %lf ",current_ori_z);
     correction(current_ori_z);
   }
@@ -115,7 +132,7 @@ void up()
   //goal.target_pose.pose.orientation.x = 0; 
   //goal.target_pose.pose.orientation.y = 0; 
   goal.target_pose.pose.orientation.z = 0; 
-  goal.target_pose.pose.orientation.w = 0.95; 
+  goal.target_pose.pose.orientation.w = 1; 
 
   ROS_INFO("correction Sending goal");
   ac.sendGoal(goal);
@@ -147,7 +164,7 @@ void down()
 
   //goal.target_pose.pose.orientation.x = 0; 
   //goal.target_pose.pose.orientation.y = 0; 
-  goal.target_pose.pose.orientation.z = -0.95; 
+  goal.target_pose.pose.orientation.z = -1; 
   goal.target_pose.pose.orientation.w = 0; 
 
   ROS_INFO("correction Sending goal");
@@ -214,8 +231,8 @@ void right()
   
   //goal.target_pose.pose.orientation.x = 0; 
   //goal.target_pose.pose.orientation.y = 0;
-  goal.target_pose.pose.orientation.z = -0.7; 
-  goal.target_pose.pose.orientation.w = 0.7; 
+  goal.target_pose.pose.orientation.z = -0.702; 
+  goal.target_pose.pose.orientation.w = 0.702; 
 
   ROS_INFO("correction Sending goal");
   ac.sendGoal(goal);
